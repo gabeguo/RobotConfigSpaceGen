f99f2204f00e8ec5560b982ccbea2b55891316fe
Gabe <gzg2104@columbia.edu>
2022-09-16 00:20:41 -0400

    1) updated heatmap in 2DOF to include straight-up binary accuracy (not just excluding uncertain points) 2) added dummy classifier to 2DOF heatmap

--- 2DOF_Heatmap/predict_points.py
+++ 2DOF_Heatmap/predict_points.py
@@ -5,8 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, \
     accuracy_score, precision_score, recall_score, f1_score
+from sklearn.dummy import DummyClassifier
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 import time
 
@@ -61,6 +63,11 @@ def plot_training_data(X, Y):
     return
 
 def plot_results(X, Y_actual, Y_confidence, Y_pred):
+    Y_pred_with_uncertain = [1 if math.isclose(y, 1) \
+                    else 0 if math.isclose(y, 0) \
+                    else UNCERTAIN \
+                    for y in Y_confidence]
+
     # convert to deg
     X = rad2deg(X)
 
@@ -80,23 +87,17 @@ def plot_results(X, Y_actual, Y_confidence, Y_pred):
     # traditional accuracy, including uncertain points
     print('accuracy with all points:', round(accuracy_score(y_true=Y_actual, y_pred=Y_pred), 3))
     # accuracy, EXCLUDING uncertain points (only have points we are confident in)
-    certain_indices = [i for i in range(len(Y_pred)) if Y_pred[i] != UNCERTAIN]
+    certain_indices = [i for i in range(len(Y_pred_with_uncertain)) if Y_pred_with_uncertain[i] != UNCERTAIN]
     certain_y_pred = [Y_pred[i] for i in certain_indices]
     certain_y_true = [Y_actual[i] for i in certain_indices]
     print('accuracy excluding uncertain points:', round(accuracy_score(y_true=certain_y_true, y_pred=certain_y_pred), 3))
     # Count number of uncertain points
     print('proportion of points that model is uncertain about:', round(1 - len(certain_indices) / len(Y_actual), 3))
-    """
-    print('precision:', round(precision_score(y_true=Y_actual, y_pred=Y_pred), 3))
-    print('recall:', round(recall_score(y_true=Y_actual, y_pred=Y_pred), 3))
-    print('f1:', round(f1_score(y_true=Y_actual, y_pred=Y_pred), 3))
-    """
-
     print()
 
     return
 
-def plot_errors(X, Y, test_size):
+def evaluate(X, Y, test_size):
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, \
         test_size=test_size, random_state=42)
 
@@ -108,26 +109,27 @@ def plot_errors(X, Y, test_size):
 
     clf.fit(X_train, Y_train)
     Y_confidence_score = clf.predict(X_test)
-    Y_binary_pred = [1 if y > 1 - MARGIN_OF_CERTAINTY \
-                    else 0 if y < MARGIN_OF_CERTAINTY \
-                    else UNCERTAIN \
-                    for y in Y_confidence_score]
-
-    #print(Y_confidence_score[:10])
+    Y_pred = [int(y + 0.5) for y in Y_confidence_score]
 
     end = time.time()
     elapsed = round(end - start, 3)
     print('time elapsed in fitting on', len(X_train), 'points and testing on', len(X_test), 'points:', elapsed, 'seconds')
 
+    # get dummy results
+    dummy = DummyClassifier(strategy="most_frequent")
+    dummy.fit(X_train, Y_train)
+    print('dummy results:', round(accuracy_score(y_true=Y_test, y_pred=dummy.predict(X_test)), 3))
+
+    # get accuracy
     plot_training_data(X_train, Y_train)
-    plot_results(X=X_test, Y_actual=Y_test, Y_confidence = Y_confidence_score, Y_pred=Y_binary_pred)
+    plot_results(X=X_test, Y_actual=Y_test, Y_confidence=Y_confidence_score, Y_pred=Y_pred)
 
     return
 
 def main():
     X, Y = read_data()
 
-    plot_errors(X, Y, test_size=0.5)
+    evaluate(X, Y, test_size=0.5)
 
     return
 
