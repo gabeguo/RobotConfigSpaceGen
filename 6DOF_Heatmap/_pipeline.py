8e20d34b1fa310f77622a1da99f80a09140e63de
Gabe <gzg2104@columbia.edu>
2022-09-19 14:29:13 -0400

    changed pipeline in 6dof

---
+++ 6DOF_Heatmap/_pipeline.py
@@ -0,0 +1,7 @@
+import six_dof_gather_collision_points
+import predict_points
+
+print('\n**\ndata gathering\n**\n')
+six_dof_gather_collision_points.main()
+print('\n\n**\nmachine learning\n**\n')
+predict_points.main()
