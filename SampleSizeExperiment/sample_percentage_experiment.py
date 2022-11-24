0ef9cdcc59e8d6648427ada4ed0f1eb3d2077e73
Gabe <gzg2104@columbia.edu>
2022-11-23 22:41:06 -0500

    saving data so i dont lose it{

--- SampleSizeExperiment/sample_percentage_experiment.py
+++ SampleSizeExperiment/sample_percentage_experiment.py
@@ -17,15 +17,15 @@ for i in nums_robots:
     robot_positions.append([radius * np.cos(theta), radius * np.sin(theta), 0])
     robot_orientations.append([0, 0, theta])
 
-simulation_res = multiple_kuka_gather_collision_points.main(NUM_ITERATIONS=5 * 1000 * 1000, NUM_ROBOTS=MAX_NUM_ROBOTS, \
-    robot_positions=robot_positions, robot_orientations=robot_orientations)
+# simulation_res = multiple_kuka_gather_collision_points.main(NUM_ITERATIONS=5 * 1000 * 1000, NUM_ROBOTS=MAX_NUM_ROBOTS, \
+#     robot_positions=robot_positions, robot_orientations=robot_orientations)
 
-for test_size in [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]:
+for test_size in [0.999, 0.998, 0.996, 0.99, 0.98, 0.95, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01, 0.001]:
     print('\n\n*** test size = {} ***\n\n'.format(test_size))
     prediction_res = predict_points.main(test_size=test_size)
-    full_res[test_size] = {**simulation_res, **prediction_res}
+    full_res[test_size] = prediction_res#{**simulation_res, **prediction_res}
 
 print(json.dumps(full_res, indent=4))
 
-with open('sample_percent_results.json', 'w') as fout:
+with open('sample_size_results_redone.json', 'w') as fout:
     json.dump(full_res, fout)
