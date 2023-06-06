81467fa24e89f27a05fd91537387cfd352d4d29b
the real gabe guo <gzg2104@columbia.edu>
2023-06-06 18:51:53 +0000

    changed margins for collision density experiment, so that we can generate robots

--- _GenerateEnvironment/run_environment_generation.sh
+++ _GenerateEnvironment/run_environment_generation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## 1) Increasing DoF experiment: impact of # DoF on Fastron performance
+echo "experiment 1: increase DoF"
 for num_robots in {1..4}
 do
     for seed in {0..2}
@@ -13,6 +14,7 @@ done
 
 ## Is decreasing performance in high DoF due to changing collision density?
 ## 2) Hold DoF constant, adjust collision density
+echo "experiment 2: adjust collision density"
 for num_obstacles in {10..60..10}
 do
     for seed in {0..2}
@@ -28,7 +30,7 @@ do
         python generate_environment.py --num_obstacles $num_obstacles --seed $seed \
         --num_robots 3 \
         --num_samples 50000 \
-        --min_robot_robot_distance 2.5 \
+        --min_robot_robot_distance 2.0 \
         --max_robot_robot_distance 5.0 \
         --keyword_name 'separateRobots'
     done
@@ -36,6 +38,7 @@ done
 
 ## Or is decreasing performance in high DoF due to undersampling (curse of dimensionality)?
 ## 3) Hold DoF and collision density constant, adjust number of samples (can just sample a lot, and then take subset later)
+echo "experiment 3: adjust number of samples"
 for seed in {0..2}
 do
     python generate_environment.py --num_samples 1000000 --seed $seed \
