e3db580653746957abec6518a22d25ac51e0dd9a
the real gabe guo <gzg2104@columbia.edu>
2023-06-06 16:26:16 +0000

    changed python script name that is called

--- _GenerateEnvironment/run_environment_generation.sh
+++ _GenerateEnvironment/run_environment_generation.sh
@@ -5,7 +5,7 @@ for num_robots in {1..4}
 do
     for seed in {0..2}
     do
-        python main.py --num_robots $num_robots --seed $seed \
+        python generate_environment.py --num_robots $num_robots --seed $seed \
         --num_obstacles 25 \
         --num_samples 50000
     done
@@ -18,14 +18,14 @@ do
     for seed in {0..2}
     do
         # 2a) Is this due to robot-robot collision?
-        python main.py --num_obstacles $num_obstacles --seed $seed \
+        python generate_environment.py --num_obstacles $num_obstacles --seed $seed \
         --num_robots 3 \
         --num_samples 50000 \
         --min_robot_robot_distance 0.5 \
         --max_robot_robot_distance 2.0 \
         --keyword_name 'collidingRobots'
         # 2b) Or robot-obstacle collision?
-        python main.py --num_obstacles $num_obstacles --seed $seed \
+        python generate_environment.py --num_obstacles $num_obstacles --seed $seed \
         --num_robots 3 \
         --num_samples 50000 \
         --min_robot_robot_distance 2.5 \
@@ -38,7 +38,7 @@ done
 ## 3) Hold DoF and collision density constant, adjust number of samples (can just sample a lot, and then take subset later)
 for seed in {0..2}
 do
-    python main.py --num_samples 1000000 --seed $seed \
+    python generate_environment.py --num_samples 1000000 --seed $seed \
     --num_obstacles 25 \
     --num_robots 3 \
     --keyword_name '${num_samples}Samples'
