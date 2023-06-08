e0ddd6ba15b1843d191c581eb4aaebc83e75eb01
the real gabe guo <gzg2104@columbia.edu>
2023-06-08 22:04:43 +0000

    have adjustable bias in DL model

--- _GenerateEnvironment/compare_models.py
+++ _GenerateEnvironment/compare_models.py
@@ -51,8 +51,6 @@ def run_fastron(args):
     data_test = all_data[first_test_index:]
     y_train = y[:args.num_training_samples]
     y_test = y[first_test_index:]
-    y_binary_train = y_binary[:args.num_training_samples]
-    y_binary_test = y_binary[first_test_index:]
 
     # sanity check
     print('inputs:', data_train[-2:])
@@ -60,7 +58,7 @@ def run_fastron(args):
     print('std of training data:', data_train.std())
 
     # Initialize Neural Network
-    dl_model = CSpaceNet(dof=data_train.shape[1], num_freq=256, sigma=5).cuda()
+    dl_model = CSpaceNet(dof=data_train.shape[1], num_freq=4, sigma=2).cuda()
 
     # Initialize PyFastron
     fastron = PyFastron(data_train) # where data.shape = (N, d)
@@ -80,7 +78,7 @@ def run_fastron(args):
         if model_name == FASTRON:
             model.updateModel()
         else:
-            train_deep_learning(model=model, X_train=data_train, Y_train=y_binary_train)
+            train_deep_learning(model=model, X_train=data_train, Y_train=y_train)
         end = time.time()
         elapsed_train = end - start
         print('time elapsed in fitting on {} points for {} dof: {:.3f} seconds'.format(len(data_train), data_train.shape[1] / (3 if args.forward_kinematics_kernel else 1), elapsed_train))
@@ -89,17 +87,21 @@ def run_fastron(args):
         start = time.time()
         if model_name == FASTRON:
             pred = model.eval(data_test) # where data_test.shape = (N_test, d) 
+            print(fastron.G.shape)
+            print(fastron.N)
         else:    
             pred = test_deep_learning(model, X_test=data_test)
+            # https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/6
+            model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+            params = sum([np.prod(p.size()) for p in model_parameters])
+            print(params)
         end = time.time()
         elapsed_test = end - start
         print('time elapsed in testing on {} points for {} dof: {:.3f} seconds'.format(len(data_test), data_test.shape[1]  / (3 if args.forward_kinematics_kernel else 1), elapsed_test))
 
         # Get metrics
-        if model_name == FASTRON:
-            cm = confusion_matrix(y_true=y_test.astype(int).flatten(), y_pred=pred.astype(int).flatten())
-        else:
-            cm = confusion_matrix(y_true=y_binary_test.astype(int).flatten(), y_pred=np.array(pred).astype(int).flatten())
+        pred = np.array(pred).astype(int)
+        cm = confusion_matrix(y_true=y_test.astype(int).flatten(), y_pred=pred.astype(int).flatten())
         print(cm)
 
         TN = cm[0][0]
@@ -165,10 +167,13 @@ def train_deep_learning(model, X_train, Y_train, learning_rate=1e-3, batch_size=
     X_train = X_train[:first_val_index]
     Y_train = Y_train[:first_val_index]
 
+    beta = 1.2
+    biases = biases = torch.Tensor([beta if curr_y > 0 else 1 for curr_y in Y_train]).cuda()
+
     percent_collision = torch.sum(Y_train == 1) / len(Y_train)
     print('percent collision:', percent_collision)
 
-    criterion = nn.L1Loss(reduction='sum')#nn.CrossEntropyLoss(weight=weights, reduction='sum')
+    criterion = nn.L1Loss(reduction='sum')
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=EPOCHS, eta_min=1e-7)
 
@@ -190,7 +195,8 @@ def train_deep_learning(model, X_train, Y_train, learning_rate=1e-3, batch_size=
             y = Y_train[idx:idx+batch_size]
 
             y_pred = model(x)
-            loss = criterion(y_pred.flatten(), y.flatten())
+            
+            loss = criterion(biases[idx:idx+batch_size] * y_pred.flatten(), y.flatten())
             
             total_loss += loss
             loss.backward()
@@ -223,8 +229,10 @@ def train_deep_learning(model, X_train, Y_train, learning_rate=1e-3, batch_size=
     return model
 
 def test_deep_learning(model, X_test):
-    X_test = torch.FloatTensor(X_test).cuda()
-    return [int(y + 0.5) for y in model(X_test[:, :]).flatten().tolist()]
+    model = model.cpu()
+    X_test = torch.FloatTensor(X_test)#.cuda()
+    return [1 if y > 0 else -1 for y in model(X_test).flatten().tolist()]
+    #return [int(y + 0.5) for y in model(X_test[:, :]).flatten().tolist()]
 
 def main():
     parser = argparse.ArgumentParser()
@@ -234,7 +242,7 @@ def main():
     parser.add_argument('--g', type=int, default=5)
     parser.add_argument('--beta', type=int, default=1)
     parser.add_argument('--maxUpdates', type=int, default=100000)
-    parser.add_argument('--maxSupportPoints', type=int, default=100000)
+    parser.add_argument('--maxSupportPoints', type=int, default=20000)
     parser.add_argument('--forward_kinematics_kernel', action='store_true')
 
     args = parser.parse_args()
