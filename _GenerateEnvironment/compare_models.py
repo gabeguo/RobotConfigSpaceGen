aae7bc4196bfde57dab8b1794390e23748467d8d
the real gabe guo <gzg2104@columbia.edu>
2023-06-14 12:08:48 +0000

    updated model comparison script

--- _GenerateEnvironment/compare_models.py
+++ _GenerateEnvironment/compare_models.py
@@ -21,18 +21,54 @@ import torch
 import torch.nn as nn
 
 import random
+from datetime import datetime
 
-"""
---num_training_samples 10000 \
---dataset_name "1robots_25obstacles_seed0_" \
---g 10 \
---beta 500 \
---maxUpdates 10000 \
---maxSupportPoints 10000 \
---forward_kinematics_kernel \
-"""
+def log_results(y_test, pred, elapsed_train, elapsed_test, args):
+    pred = np.array(pred).astype(int)
+    cm = confusion_matrix(y_true=y_test.astype(int).flatten(), y_pred=pred.astype(int).flatten())
+    print(cm)
 
-def run_fastron(args):
+    TN = cm[0][0]
+    FN = cm[1][0]
+    TP = cm[1][1]
+    FP = cm[0][1]
+
+    # traditional accuracy, including uncertain points
+    acc = (TP+TN)/(TP+TN+FP+FN)
+    tpr = TP/(TP+FN)
+    tnr = TN/(TN+FP)
+    print()
+    print('accuracy:', round(acc, 3))
+    print('sensitivity (TPR):', round(tpr, 3))
+    print('specificity (TNR):', round(tnr, 3))
+
+    # log results & args
+    results = {
+        ACCURACY: acc,
+        TPR: tpr,
+        TNR: tnr,
+        TP_NAME: int(TP),
+        TN_NAME: int(TN),
+        FP_NAME: int(FP),
+        FN_NAME: int(FN), 
+        TRAIN_TIME:elapsed_train,
+        TEST_TIME:elapsed_test,
+        TRAIN_SIZE:args.num_training_samples,
+        TEST_SIZE:y_test.flatten().shape[0],
+    }
+    results.update(vars(args)) # update with args
+
+    now = datetime.now()
+    # Format the date and time as a string in the format 'yy_mm_dd_hh_mm_ss'
+    formatted_timestamp = now.strftime('%y_%m_%d_%H_%M_%S')
+
+    os.makedirs(RESULTS_FOLDER, exist_ok=True)
+    filename = '{}/{}_{}.json'.format(
+        RESULTS_FOLDER, args.model_name, formatted_timestamp)
+    with open(filename, 'w') as f:
+        json.dump(results, f, indent=4)
+
+def run_model(args):
     # load data
     if args.forward_kinematics_kernel:
         config_filename = '{}/linkPositions_{}.npy'.format(DATA_FOLDER, args.dataset_name)
@@ -60,109 +96,67 @@ def run_fastron(args):
     print('std of training data:', data_train.std())
 
     # Initialize Neural Network
-    dl_model = CSpaceNet(dof=data_train.shape[1], num_freq=8, sigma=1).cuda()
+    if args.model_name == DL:
+        model = CSpaceNet(dof=data_train.shape[1], num_freq=args.num_freq, sigma=args.sigma).cuda()
+    elif args.model_name == FASTRON:
+        # Initialize PyFastron
+        model = PyFastron(data_train) # where data.shape = (N, d)
 
-    # Initialize PyFastron
-    fastron = PyFastron(data_train) # where data.shape = (N, d)
+        model.y = y_train # where y.shape = (N,)
 
-    fastron.y = y_train # where y.shape = (N,)
+        model.g = args.g # from paper for high DOF: 5
+        model.maxUpdates = args.maxUpdates
+        model.maxSupportPoints = args.maxSupportPoints
+        model.beta = args.beta # from paper for high DOF: 500
+    else:
+        raise ValueError('unsupported model type')
 
-    fastron.g = args.g # from paper for high DOF: 5
-    fastron.maxUpdates = args.maxUpdates
-    fastron.maxSupportPoints = args.maxSupportPoints
-    fastron.beta = args.beta # from paper for high DOF: 500
+    print('\n', args.model_name)
 
-    for model_name, model in zip([DL], [dl_model]):#zip([FASTRON, DL], [fastron, dl_model]):
-        print('\n', model_name)
+    # Train model
+    start = time.time()
+    if args.model_name == FASTRON:
+        model.updateModel()
+    else:
+        train_deep_learning(model=model, X_train=data_train, Y_train=y_train, args=args)
+    end = time.time()
+    elapsed_train = end - start
+    print('time elapsed in fitting on {} points for {} dof: {:.3f} seconds'.format(len(data_train), data_train.shape[1] / (3 if args.forward_kinematics_kernel else 1), elapsed_train))
 
-        # Train model
-        start = time.time()
-        if model_name == FASTRON:
-            model.updateModel()
-        else:
-            train_deep_learning(model=model, X_train=data_train, Y_train=y_train)
-        end = time.time()
-        elapsed_train = end - start
-        print('time elapsed in fitting on {} points for {} dof: {:.3f} seconds'.format(len(data_train), data_train.shape[1] / (3 if args.forward_kinematics_kernel else 1), elapsed_train))
+    # Predict values for a test set
+    start = time.time()
+    if args.model_name == FASTRON:
+        pred = model.eval(data_test) # where data_test.shape = (N_test, d) 
+        print(model.G.shape)
+        print(model.N)
+    else:    
+        pred = test_deep_learning(model, X_test=data_test)
+    end = time.time()
+    elapsed_test = end - start
+    print('time elapsed in testing on {} points for {} dof: {:.3f} seconds'.format(len(data_test), data_test.shape[1]  / (3 if args.forward_kinematics_kernel else 1), elapsed_test))
 
-        # Predict values for a test set
-        start = time.time()
-        if model_name == FASTRON:
-            pred = model.eval(data_test) # where data_test.shape = (N_test, d) 
-            print(fastron.G.shape)
-            print(fastron.N)
-        else:    
-            pred = test_deep_learning(model, X_test=data_test)
-            # https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/6
-            model_parameters = filter(lambda p: p.requires_grad, model.parameters())
-            params = sum([np.prod(p.size()) for p in model_parameters])
-            print(params)
-        end = time.time()
-        elapsed_test = end - start
-        print('time elapsed in testing on {} points for {} dof: {:.3f} seconds'.format(len(data_test), data_test.shape[1]  / (3 if args.forward_kinematics_kernel else 1), elapsed_test))
+    # Get metrics
+    log_results(y_test=y_test, pred=pred, elapsed_train=elapsed_train, elapsed_test=elapsed_test, args=args)
 
-        # Get metrics
-        pred = np.array(pred).astype(int)
-        cm = confusion_matrix(y_true=y_test.astype(int).flatten(), y_pred=pred.astype(int).flatten())
-        print(cm)
+    # Sanity check
+    assert args.num_training_samples == data_train.shape[0]
 
-        TN = cm[0][0]
-        FN = cm[1][0]
-        TP = cm[1][1]
-        FP = cm[0][1]
+    # Get number of params in DL model
+    if args.model_name == DL:
+        # https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/6
+        model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+        params = sum([np.prod(p.size()) for p in model_parameters])
+        print(params)
 
-        # traditional accuracy, including uncertain points
-        acc = (TP+TN)/(TP+TN+FP+FN)
-        tpr = TP/(TP+FN)
-        tnr = TN/(TN+FP)
-        print()
-        print('accuracy:', round(acc, 3))
-        print('sensitivity (TPR):', round(tpr, 3))
-        print('specificity (TNR):', round(tnr, 3))
-
-        # log results & args
-        assert args.num_training_samples == data_train.shape[0]
-        results = {
-            ACCURACY: acc,
-            TPR: tpr,
-            TNR: tnr,
-            TP_NAME: int(TP),
-            TN_NAME: int(TN),
-            FP_NAME: int(FP),
-            FN_NAME: int(FN), 
-            TRAIN_TIME:elapsed_train,
-            TEST_TIME:elapsed_test,
-            TRAIN_SIZE:args.num_training_samples,
-            TEST_SIZE:data_test.shape[0],
-        }
-        results.update(vars(args)) # update with args
-
-        os.makedirs(RESULTS_FOLDER, exist_ok=True)
-        filename = '{}/{}Results_{}_{}_{}_{}_{}.json'.format(
-            RESULTS_FOLDER, 
-            model_name,
-            'forwardKinematics' if args.forward_kinematics_kernel else 'normalizedAngles',
-            f'{args.num_training_samples}Samples',
-            f'{args.beta}Beta',
-            f'{args.g}Gamma',
-            args.dataset_name)
-        with open(filename, 'w') as f:
-            json.dump(results, f, indent=4)
     return
 
-def train_deep_learning(model, X_train, Y_train, learning_rate=1e-3, batch_size=512, train_percent=0.95):
-    #model = CSpaceNet(dof=7*4, num_freq=128, sigma=1.5).cuda()
-
-    #print(model)
-
+def train_deep_learning(model, X_train, Y_train, args):
     #X_train = (X_train - X_train.mean())/X_train.std()
-
-    EPOCHS = 50
 
     best_val_loss = 1e6
     best_epoch = -1
 
-    first_val_index = int(len(Y_train) * train_percent)
+    first_val_index = int(len(Y_train) * args.train_percent)
     X_train = torch.FloatTensor(X_train).cuda()
     Y_train = torch.FloatTensor(Y_train).cuda()
 
@@ -172,35 +166,35 @@ def train_deep_learning(model, X_train, Y_train, learning_rate=1e-3, batch_size=
     Y_train = Y_train[:first_val_index]
 
     beta = 1
-    biases = biases = torch.Tensor([beta if curr_y > 0 else 1 for curr_y in Y_train]).cuda()
+    biases = torch.Tensor([beta if curr_y > 0 else 1 for curr_y in Y_train]).cuda()
 
     percent_collision = torch.sum(Y_train == 1) / len(Y_train)
     print('percent collision:', percent_collision)
 
     criterion = nn.L1Loss(reduction='sum')
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=EPOCHS, eta_min=1e-7)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=0)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs, eta_min=1e-7)
 
-    for i in range(EPOCHS):
+    for i in range(args.epochs):
         total_loss = 0
 
         # train
         model.train()
 
-        indices = list(range(0, len(X_train), batch_size))
+        indices = list(range(0, len(X_train), args.batch_size))
         #print(indices)
         random.shuffle(indices)
         for idx in indices:
             #print(idx)
             model.zero_grad()
 
-            x = X_train[idx:idx+batch_size, :]
+            x = X_train[idx:idx+args.batch_size, :]
             #print(x.shape)
-            y = Y_train[idx:idx+batch_size]
+            y = Y_train[idx:idx+args.batch_size]
 
             y_pred = model(x)
             
-            loss = criterion(biases[idx:idx+batch_size] * y_pred.flatten(), y.flatten())
+            loss = criterion(biases[idx:idx+args.batch_size] * y_pred.flatten(), y.flatten())
             
             total_loss += loss
             loss.backward()
@@ -212,9 +206,9 @@ def train_deep_learning(model, X_train, Y_train, learning_rate=1e-3, batch_size=
         model.eval()
 
         total_val_loss = 0
-        for idx in range(0, len(Y_val), batch_size):
-            x = X_val[idx:idx+batch_size, :]
-            y = Y_val[idx:idx+batch_size]
+        for idx in range(0, len(Y_val), args.batch_size):
+            x = X_val[idx:idx+args.batch_size, :]
+            y = Y_val[idx:idx+args.batch_size]
             val_loss = criterion(model(x).flatten(), y.flatten())
             total_val_loss += val_loss
         if i % 5 == 0:
@@ -242,17 +236,29 @@ def test_deep_learning(model, X_test):
 def main():
     parser = argparse.ArgumentParser()
 
+    # which model to pick
+    parser.add_argument('--model_name', type=str, default=DL)
+    # general experimental params
     parser.add_argument('--num_training_samples', type=int, default=30000)
     parser.add_argument('--dataset_name', type=str, default="3robots_25obstacles_seed0_")
+    parser.add_argument('--forward_kinematics_kernel', action='store_true')
+    # fastron-specific params
     parser.add_argument('--g', type=int, default=5)
     parser.add_argument('--beta', type=int, default=1)
     parser.add_argument('--maxUpdates', type=int, default=100000)
     parser.add_argument('--maxSupportPoints', type=int, default=20000)
-    parser.add_argument('--forward_kinematics_kernel', action='store_true')
+    # dl-specific params
+    parser.add_argument('--bias', type=int, default=1)
+    parser.add_argument('--num_freq', type=int, default=8)
+    parser.add_argument('--sigma', type=float, default=1)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--batch_size', type=int, default=512)
+    parser.add_argument('--train_percent', type=float, default=0.95)
+    parser.add_argument('--epochs', type=int, default=50)
 
     args = parser.parse_args()
 
-    run_fastron(args)
+    run_model(args)
 
 if __name__ == '__main__':
     main()
