e5dc1f65d220360d1702fa3ae25d8e33eb4aa976
the real gabe guo <gzg2104@columbia.edu>
2023-06-17 23:09:49 +0000

    updated run num obstacles experiment

--- _GenerateEnvironment/run_num_obstacles_eval.sh
+++ _GenerateEnvironment/run_num_obstacles_eval.sh
@@ -6,42 +6,45 @@ for num_obstacles in {10..60..10}
 do
     for seed in 0 1 2
     do
-        dataset_name="3robots_${num_obstacles}obstacles_seed${seed}_"
-
-        # Fastron
-        for support in 3000 10000 30000
+        for environment_type in "colliding" "separate"
         do
-            for updates in 5000 30000
+            dataset_name="3robots_${num_obstacles}obstacles_seed${seed}_${environment_type}Robots"
+
+            # Fastron
+            for support in 3000 10000 30000
             do
-                for g in 1 5 10
+                for updates in 5000 30000
                 do
-                    for b in 1 500 1000
+                    for g in 1 5 10
                     do
-                        echo "Fastron: support ${support}, updates ${updates}, g ${g}, b ${b}"
-                        python compare_models.py --model_name 'Fastron' --forward_kinematics_kernel \
-                            --num_training_samples $num_training_samples \
-                            --dataset_name $dataset_name \
-                            --g $g --beta $b --maxUpdates $updates --maxSupportPoints $support \
-                            --results_folder $results_folder
+                        for b in 1 500 1000
+                        do
+                            echo "Fastron: support ${support}, updates ${updates}, g ${g}, b ${b}"
+                            python compare_models.py --model_name 'Fastron' --forward_kinematics_kernel \
+                                --num_training_samples $num_training_samples \
+                                --dataset_name $dataset_name \
+                                --g $g --beta $b --maxUpdates $updates --maxSupportPoints $support \
+                                --results_folder $results_folder
+                        done
                     done
                 done
             done
-        done
 
-        # DL
-        for freq in 4 8 12
-        do
-            for b in 1 2 5
+            # DL
+            for freq in 4 8 12
             do
-                for sigma in 0.5 1 2
+                for b in 1 2 5
                 do
-                    echo "DL: freq ${freq}, b ${b}, sigma ${sigma}"
-                    python compare_models.py --model_name 'DL' --forward_kinematics_kernel \
-                        --num_training_samples $num_training_samples \
-                        --dataset_name $dataset_name \
-                        --bias $b --num_freq $freq --sigma $sigma \
-                        --lr 1e-3 --batch_size 512 --train_percent 0.95 --epochs 50 \
-                        --results_folder $results_folder
+                    for sigma in 0.5 1 2
+                    do
+                        echo "DL: freq ${freq}, b ${b}, sigma ${sigma}"
+                        python compare_models.py --model_name 'DL' --forward_kinematics_kernel \
+                            --num_training_samples $num_training_samples \
+                            --dataset_name $dataset_name \
+                            --bias $b --num_freq $freq --sigma $sigma \
+                            --lr 1e-3 --batch_size 512 --train_percent 0.95 --epochs 50 \
+                            --results_folder $results_folder
+                    done
                 done
             done
         done
