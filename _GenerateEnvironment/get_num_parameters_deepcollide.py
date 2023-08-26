4f0543465f08642e1a6689991c1c7a1e7f2f5758
the real gabe guo <gzg2104@columbia.edu>
2023-08-26 14:36:19 +0000

    have code to plot the number of parameters, as it relates to DoF

--- _GenerateEnvironment/get_num_parameters_deepcollide.py
+++ _GenerateEnvironment/get_num_parameters_deepcollide.py
@@ -2,9 +2,9 @@ import numpy as np
 from cspace_net import CSpaceNet
 import matplotlib.pyplot as plt
 
-def get_dependence_on_dof(dof, num_freq):
-
-    model = CSpaceNet(dof=42*3, num_freq=12, sigma=2)
+def get_dependence_on_dof(num_coordinates, num_freq):
+    # dof is actually number of inputs aka coordinates (= 3 * DoF, with FK kernel)
+    model = CSpaceNet(dof=num_coordinates, num_freq=num_freq, sigma=2)
 
     # https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/6
     model_parameters = [x for x in filter(lambda p: p.requires_grad, model.parameters())]
@@ -15,7 +15,7 @@ def get_dependence_on_dof(dof, num_freq):
     #     print(p.size())
 
     total_num_params = sum([np.prod(p.size()) for p in model_parameters])
-    print('total:', total_num_params)
+    # print('total:', total_num_params)
 
     # total_num_dependent_params = sum([np.prod(p.size()) for p in dependent_parameters_on_dof]) - (256 * 256)
     # print('dependent on DoF:', total_num_dependent_params)
@@ -25,3 +25,24 @@ def get_dependence_on_dof(dof, num_freq):
 
     return total_num_params
 
+if __name__ == "__main__":
+    num_freq = 12 # max number of frequencies we test in the paper
+
+    joint_nums = [7, 14, 21, 28, 35, 42]
+    param_nums = []
+
+    for joints in joint_nums:
+        num_coordinates = 3 * joints # from FK kernel
+
+        num_params = get_dependence_on_dof(num_coordinates=num_coordinates, num_freq=num_freq)
+        param_nums.append(num_params)
+
+        print(joints, num_params)
+    
+    plt.plot(joint_nums, param_nums)
+    plt.grid()
+    plt.ylim(0, max(param_nums))
+
+    plt.xticks(joint_nums)
+
+    plt.show()
