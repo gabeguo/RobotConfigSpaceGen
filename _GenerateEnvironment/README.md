f36d818adaf610e361414f950a870b367d9948bd
gabeguo <gzg2104@columbia.edu>
2023-09-06 20:00:19 -0400

    have setup instructions

---
+++ _GenerateEnvironment/README.md
@@ -0,0 +1,3 @@
+Python 3.9.16
+
+Run setup.sh
---
+++ _GenerateEnvironment/setup.sh
@@ -0,0 +1,7 @@
+# pip install -r requirements.txt
+
+cd ../
+# git clone https://github.com/ucsdarclab/fastron_python.git
+
+cd fastron_python
+python setup.py build_ext --inplace
