55082111c5b7076194c69649038537ec9292b2c0
Gabe Guo <gzg2104@columbia.edu>
2024-07-13 09:42:04 -0400

    have sampling graphing code

--- _GenerateEnvironment/graph_sampling.sh
+++ _GenerateEnvironment/graph_sampling.sh
@@ -1,11 +1,11 @@
 python graph_sample_experiment.py --save_location sampling_results \
-    --data_directory surfaceSampling_experiment_results_moreDoF \
+    --data_directory surfaceSampling_experiment_results \
     --metric accuracy
 
 python graph_sample_experiment.py --save_location sampling_results \
-    --data_directory surfaceSampling_experiment_results_moreDoF \
+    --data_directory surfaceSampling_experiment_results \
     --metric tpr
 
 python graph_sample_experiment.py --save_location sampling_results \
-    --data_directory surfaceSampling_experiment_results_moreDoF \
+    --data_directory surfaceSampling_experiment_results \
     --metric tnr
